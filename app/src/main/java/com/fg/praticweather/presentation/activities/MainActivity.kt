cf3e9eb8621df90e865a6fef0cdf791ceb65d9cb
Furkan Gencer <furkan_gencer1@outlook.com>
2023-11-27 17:22:54 +0300

    Update

--- app/src/main/java/com/fg/praticweather/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/fg/praticweather/presentation/activities/MainActivity.kt
@@ -4,11 +4,14 @@ package com.fg.praticweather.presentation.activities
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
+import com.fg.praticweather.R
 import com.fg.praticweather.databinding.ActivityMainBinding
 
 
-class  MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +20,7 @@ class  MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
     }
 }
 
